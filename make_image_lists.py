437f38a9527af330db56234a14663da2a257fea2
rmsare-lanl <rmsare@lanl.gov>
2018-07-24 13:30:11 -0600

    Format to pass flake8

--- make_image_lists.py
+++ make_image_lists.py
@@ -13,10 +13,11 @@ def parse_filename(filename):
     month, day, year = strings[2:5]
     path = int(strings[5][0:3])
     row = int(strings[5][3:7])
-    in_date = datetime.strptime('{} {} {}'.format(month, day, year), 
+    in_date = datetime.strptime('{} {} {}'.format(month, day, year),
                                 '%b %d %Y')
     out_date = in_date.strftime('%Y-%m-%d')
     return platform, path, row, out_date
+
 
 def process_directory(directory):
     out_filename = os.path.join(directory, 'imagelist.txt')
@@ -28,6 +29,7 @@ def process_directory(directory):
         for f in files:
             platform, path, row, date = parse_filename(f)
             out.write('{} {} {} {}\n'.format(platform, path, row, date))
+
 
 if __name__ == "__main__":
     base_directory = '/Users/rmsare/data/cleaned/Ucayali/images/'
--- process_legacy_data.py
+++ process_legacy_data.py
@@ -14,7 +14,6 @@ STRUCT_FIELD_NAME = 'cmap'
 
 
 def get_raster_profile(directory):
-    region_name = directory.split('/')[-1]
     files = os.listdir(directory)
     available_tif_files = [f for f in files if '.tif' in f]
     in_file = available_tif_files[0]
@@ -24,11 +23,13 @@ def get_raster_profile(directory):
         profile.update(count=1)
     return profile
 
+
 def read_data(filename, shape):
     struct = loadmat(filename)
     data = np.array(struct[STRUCT_FIELD_NAME])
     data = data.reshape(shape, order='F')
     return data
+
 
 def save_file_as_tiff(in_filename, profile, shape):
     out_filename = in_filename.replace('mat', 'tif')
@@ -36,10 +37,11 @@ def save_file_as_tiff(in_filename, profile, shape):
     with rasterio.open(out_filename, 'w', **profile) as out:
         out.write(data.astype(profile['dtype']), 1)
 
+
 def process_directory(directory):
     profile = get_raster_profile(directory)
     shape = (profile['height'], profile['width'])
-    
+
     all_files = os.listdir(directory)
     in_files = [f for f in all_files if '.mat' in f and f[0] == 'C']
     done_files = [f for f in all_files if '.tif' in f and f[0] == 'C']
@@ -50,12 +52,14 @@ def process_directory(directory):
         filename = os.path.join(directory, f)
         save_file_as_tiff(filename, profile, shape)
 
+
 def main():
     directories = ['R3', 'R4', 'R5', 'R6']
     for directory in directories:
         print('Processing {}...'.format(directory))
         directory = os.path.join(IMAGERY_PATH, directory)
         process_directory(directory)
+
 
 if __name__ == "__main__":
     main()
